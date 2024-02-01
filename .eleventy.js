7518994bc6994dfb64d3d792d86475568ddc63e5
Lyza Danger Gardner <lyza@lyza.com>
2024-02-01 09:58:48 -0500

    Integrate tailwind
    Add tailwind to the dev server script.
    
    Needs FOLO:
    
    - Possible multiple rebuild issue when making changes to watched
      11ty template files on dev server, but debug is not easy
    - There is no `build` script yet, and this will need to include TW

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+  // Necessary because `_tmp_ is gitignored and 11ty won't see it otherwise
+  eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addWatchTarget("./_tmp/styles.css");
+  eleventyConfig.addPassthroughCopy({ "./_tmp/styles.css": "./styles.css" });
   return {
     dir: {
       input: "src",
---
+++ styles/tailwind.config.js
@@ -0,0 +1,57 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const customColors = {
+  pank: "#e60a62",
+};
+
+module.exports = {
+  content: ["src/_includes/**/*.{html,md,njk}"],
+  theme: {
+    extend: {
+      borderColor: {
+        DEFAULT: colors.stone["800"],
+      },
+      borderWidth: {
+        3: "3px",
+        10: "10px",
+      },
+      colors: {
+        ...customColors,
+        grey: colors.stone,
+      },
+      fontFamily: {
+        body: ["'Georgia'", "'Times New Roman'", "serif"],
+        title: ["'Playfair Display SC'", "'Times New Roman'", "serif"],
+        display: [
+          '"Playfair Display"',
+          "Georgia",
+          "'Times New Roman'",
+          "ui-serif",
+        ],
+      },
+      screens: {
+        xs: "480px",
+        ...defaultTheme.screens,
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: colors.sky["800"],
+              transitionProperty: "color",
+              transitionDuration: "150ms",
+              "&:hover": {
+                color: customColors.pank,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};

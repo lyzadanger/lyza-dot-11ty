a7cefb65e553d949f8efacc5091891f08a5ca830
Lyza Danger Gardner <lyza@lyza.com>
2024-02-04 11:04:04 -0500

    Establish basic header and content layout
    - Allow pass-through static images
    - Set up a header partial
    - Get CSS fonts sorted

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,14 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./_tmp/styles.css");
   eleventyConfig.addPassthroughCopy({ "./_tmp/styles.css": "./styles.css" });
+
+  // Static images
+  eleventyConfig.addPassthroughCopy("src/images");
+
   return {
     dir: {
       input: "src",
+      includes: "_includes",
     },
   };
 };

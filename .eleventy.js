78fc1821baa9b116ddabe224a009f3c144b5e3fe
Lyza Danger Gardner <lyza@lyza.com>
2024-02-05 19:07:25 -0500

    Establish tag pages

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,33 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "/" });
 
+  // Return all the tags used in a collection
+  eleventyConfig.addFilter("getAllTagsWithCount", (collection) => {
+    const tags = [];
+    for (let item of collection) {
+      (item.data.tags || []).forEach((tag) => {
+        if (tag === "post") {
+          return;
+        }
+        const existingTag = tags.find((el) => el.tag === tag);
+        if (existingTag) {
+          existingTag.count++;
+        } else {
+          tags.push({ tag, count: 1 });
+        }
+      });
+    }
+    tags.sort((a, b) => {
+      if (a.count < b.count) {
+        return 1;
+      } else if (a.count > b.count) {
+        return -1;
+      }
+      return 0;
+    });
+    return tags;
+  });
+
   return {
     dir: {
       input: "src",

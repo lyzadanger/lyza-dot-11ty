e67a0dc704192cd0f80e2e2e26a463da3925a4d6
Lyza Danger Gardner <lyza@lyza.com>
2024-02-08 12:55:54 -0500

    Install jinja prettier plugin
    Vs. liquid

--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,12 @@ module.exports = {
   // There is no prettier support for .njk. Instead, fake it by treating
   // .njk files as liquid.
   // See https://github.com/tailwindlabs/tailwindcss/discussions/11731#discussioncomment-7925745
-  plugins: ["@shopify/prettier-plugin-liquid", "prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-jinja-template", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.njk",
       options: {
-        parser: "liquid-html",
+        parser: "jinja-template",
       },
     },
   ],

c67143cde9851ee5c947a5c109d8e40afa18e017
Lyza Danger Gardner <lyza@lyza.com>
2024-02-08 09:51:49 -0500

    Install prettier plugin for liquid and use on njk templates
    Whew, I hope this workaround...works. There is no prettier support for
    .njk and I am tearing my hair out. There is, however, liquid support,
    and since .njk syntax is similar enough, the `overrides` config for
    prettier will parse .njk files with the liquid parser. This seems to
    work.

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // There is no prettier support for .njk. Instead, fake it by treating
+  // .njk files as liquid.
+  // See https://github.com/tailwindlabs/tailwindcss/discussions/11731#discussioncomment-7925745
+  plugins: ["@shopify/prettier-plugin-liquid"],
+  overrides: [
+    {
+      files: "*.njk",
+      options: {
+        parser: "liquid-html",
+      },
+    },
+  ],
+};

3f6f81b85d0ae4edc69e9d906433ae8cd6acf0eb
Lyza Danger Gardner <lyza@lyza.com>
2024-02-18 10:21:37 -0500

    Use `'posts'` instead of `'post'` for blog post collection tag
    I prefer `'post'` but the prevailing convention with 11ty is to use
    `'posts'` and I want to avoid bugs.
    
    Fixes #28
    Fixes #36

--- src/content/posts/posts.11tydata.js
+++ src/content/posts/posts.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tags: "post",
+  tags: "posts",
   layout: "layouts/blog-post.njk",
   permalink: "/{{ page.date | date: '%Y/%m/%d' }}/{{ page.fileSlug }}/",
   "tags-works-comment": "make files available in `collections.posts`",

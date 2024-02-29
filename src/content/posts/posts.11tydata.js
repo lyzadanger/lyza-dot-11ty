45c620f5855b216f9483bfe58873d3c5f5e90c97
Lyza Danger Gardner <lyza@lyza.com>
2024-02-29 10:50:37 -0500

    Refactor and rename partials (macros)
    Encapsulate partials where possible to constrain their context, and
    rename to something a little more sensible to reason about.

--- src/content/posts/posts.11tydata.js
+++ src/content/posts/posts.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
   tags: "posts",
-  layout: "layouts/blog-post.njk",
+  layout: "layouts/post.njk",
   permalink: "/{{ page.date | date: '%Y/%m/%d' }}/{{ page.fileSlug }}/",
   "tags-works-comment": "make files available in `collections.posts`",
 };

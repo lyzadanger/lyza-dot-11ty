40ddd55be1c41dd965fef16996f05f50204c92a2
Lyza Danger Gardner <lyza@lyza.com>
2024-03-14 12:58:28 -0400

    Add last-round changes to compiled TS
    These changes were made to source TS before this commit, but the watch
    task was not properly picking up TS changes.

--- src/_lib/types.js
+++ src/_lib/types.js
@@ -1,2 +1,5 @@
 "use strict";
+// Content Data types
+// `*ContentData` data are declared (in front matter, e.g.) at the most
+// granular level: a single piece of content.
 Object.defineProperty(exports, "__esModule", { value: true });

e0baa3bf912efa0f71de63776625398068ba3fed
Lyza Danger Gardner <lyza@lyza.com>
2024-03-14 16:15:17 -0400

    Use `ELEVENTY_ENV` instead of `NODE_ENV`
    I was surprised to find that netlify doesn't set `NODE_ENV` for me in
    production, so let's do this more custom-like.

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,5 +1,5 @@
 // Non-sensitive technical site config
 module.exports = {
   cloudinary: { id: "dfsssdwbu" },
-  environment: process.env.NODE_ENV || "development",
+  environment: process.env.ELEVENTY_ENV || "production",
 };
